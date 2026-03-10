3ce7bca794a8ffca304bc8d09c58572cd1d67eda
Bautista Cremona <bautistacremona@gmail.com>
2026-03-10 14:05:48 -0300

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bautistacremona.github.io',
+  base: '/phone-specs',
   vite: {
     plugins: [tailwindcss()]
   }
