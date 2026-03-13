b2c4ef08a8901e004e9c17259eadebd71bf9a90d
Bautista Cremona <bautistacremona@gmail.com>
2026-03-13 13:19:01 -0300

    config for amplify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bautistacremona.github.io',
-  base: '/phone-specs',
   vite: {
     plugins: [tailwindcss()]
   }
